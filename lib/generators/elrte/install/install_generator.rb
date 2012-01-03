0e24a3aa074febd37ecfc48ecbf6e59712e04be6
Anton <antonzaycev55@gmail.com>
2012-01-03 16:22:52 +0700

    fixed generator creates an empty folder for elfinder

--- lib/generators/elrte/install/install_generator.rb
+++ lib/generators/elrte/install/install_generator.rb
@@ -3,20 +3,24 @@ module Elrte
     class InstallGenerator < Rails::Generators::Base
       desc "Installs Elrte and generates necessary migrations"
 
+      def self.source_root
+        File.expand_path("../templates", __FILE__)
+      end
+
       #def copy_initializer
       #  template 'elrte.rb.erb', 'config/initializers/elrte.rb'
       #end
       
-      def setup_routes
-        route "Elrte.routes(self)"
-      end
-
-      def create_assets
-        generate "elrte:assets"
-      end
+      #def setup_routes
+      #  route "Elrte.routes(self)"
+      #end
+      #
+      #def create_assets
+      #  generate "elrte:assets"
+      #end
 
       def create_public_folder
-        directory "#{File.expand_path("../templates", __FILE__)}/empty_folder/system/elfinder", 'public/system/elfinder'
+        directory 'empty_folder/system/elfinder', 'public/system/elfinder'
       end
 
     end
